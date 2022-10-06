e26fe87590e0856f6409dda5000fd1e5f92c8f82
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-10-05 20:32:55 -0400

    add explantion to task Reverse a String.js

--- Reverse a String.js
+++ Reverse a String.js
@@ -34,6 +34,11 @@ reverseString1("hello");
 
 
 // or
+//Our first step is to split the string by characters using split(''). Notice that we don’t leave anything in between the single quotes, this tells the function to split the string by each character. Using the split() function will turn our string into an array of characters, keep that in mind as we move forward.
+
+// Next we chain the reverse() function, which takes our array of characters and reverses them.
+
+//Finally, we chain join('') to put our characters back together into a string. Notice once again that we left no spaces in the argument for join, this makes sure that the array of characters is joined back together by each character.
 
 function reverseString2(str) {
     return str
